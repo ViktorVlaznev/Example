e3fabb9e42f7c477fa7d34338aaeee1dba7dddab
Vlaznev Victor <v-vlaznev@mail.ru>
2022-08-20 16:24:26 +0300

    Add Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 8, 3, 4, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 8;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
